0729374190e65c11a637c5b0ba588c9914b50283
Claudia Ondecker <claudiaondecker@icloud.com>
2019-04-02 01:24:56 -0400

    Initial Commit

---
+++ ReticleV1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReticleV1
+//
+//  Created by Claudia Ondecker on 4/2/19.
+//  Copyright © 2019 Claudia Ondecker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
